964338209c7daeedd059a602ae517fccf29c74e0
Rachsana <rrachnapatel137@gmail.com>
2026-06-09 00:31:25 +0530

    initialised

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express=require("express")
+const cors=require("cors")
+const helmet=require("helmet")
+const rateLimit=require("express-rate-limit")
+const dotenv=require("dotenv")
+const prisma=require("./utils/prismaClient")
+
+dotenv.config()
+
+const app=express()
+const PORT = process.env.PORT || 3000
+
+app.use(helmet())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials:true,
+}))
+app.use(express.json())
+
+prisma.$connect().then(()=> console.log("Databas Connected"))
+.catch((err)=> console.error("Database connection failed:",err.message))
+
+const limiter = rateLimit({
+    windows: 15*60* 1000,
+    max: 100,
+    message: {error: "Too many requests, slow down"},
+})
+app.use(limiter)
+
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Hey Rachna :) track ur progress  ",
+        status:"ok",
+        timestamp: new Date().toISOString()
+    })
+})
+
+
+
+
+app.use((err,req,res,next)=>{
+    console.error("Error:",err.message);
+    res.status(err.status || 500).json({ error: err.message || "Internal server error"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
